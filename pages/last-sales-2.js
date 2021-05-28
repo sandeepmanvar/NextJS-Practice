f123667ac00b61ea8007405b385149e9a2787f87
Sandeep M <sandeep.manvar@outlook.com>
2021-05-28 09:03:22 +0530

    Initial commit with page re-rendering and data fetching practice

---
+++ pages/last-sales-2.js
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+function LastSalesPage(props) {
+  const [sales, setSales] = useState();
+
+  const { data, error } = useSWR(
+    "https://nextjs-course-ac28d-default-rtdb.firebaseio.com/sales.json"
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>Failed to load.</p>;
+  }
+
+  if (!data || !sales) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.username} - ${sale.volume}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default LastSalesPage;
